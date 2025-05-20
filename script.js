9bddd739ec54423f566f857507f5dc38cfb068a2
CodeMaestro786 <153513766+CodeMaestro786@users.noreply.github.com>
2025-05-20 09:16:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,22 @@ function showContact() {
     document.getElementById('main-content').querySelectorAll('section').forEach(section => section.style.display = 'none');
     document.getElementById('contact').style.display = 'block';
 }
+
+const menuToggle = document.getElementById('mobile-menu');
+const nav = document.querySelector('.nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+function showSection(section) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach((sec) => {
+        sec.style.display = 'none';
+    });
+
+    const activeSection = document.getElementById(section);
+    if (activeSection) {
+        activeSection.style.display = 'block';
+    }
+}
